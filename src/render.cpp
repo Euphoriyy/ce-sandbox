eab366204a2684d2de1e2bb6867ddb389ff2add3
Euphoriyy <96319862+Euphoriyy@users.noreply.github.com>
2025-04-26 19:10:26 -0400

    feat: make cursor react to brush size

--- src/render.cpp
+++ src/render.cpp
@@ -55,10 +55,12 @@ void render()
 
     // Draw Cursor
     gfx_SetColor(cursor.color);
-    uint16_t cx = cursor.pos.x * cursor.size + cursor.size / 2;
-    uint16_t cy = cursor.pos.y * cursor.size + GUI_HEIGHT + cursor.size / 2;
-    gfx_HorizLine(cx - cursor.size, cy, cursor.size * 2);
-    gfx_VertLine(cx, cy - cursor.size, cursor.size * 2);
+    // A brush size of 2 otherwise causes the cursor to be off-center
+    uint16_t cx = cursor.pos.x * cursor.size + cursor.size / (2 * brushSize) - (brushSize == 2);
+    uint16_t cy =
+        cursor.pos.y * cursor.size + GUI_HEIGHT + cursor.size / (2 * brushSize) - (brushSize == 2);
+    gfx_HorizLine(cx - cursor.size * brushSize, cy, cursor.size * 2 * brushSize);
+    gfx_VertLine(cx, cy - cursor.size * brushSize, cursor.size * 2 * brushSize);
 
     // Draw GUI
     gfx_SetColor(50);
