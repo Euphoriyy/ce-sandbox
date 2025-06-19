594c7ac1e2ebf975b213ba1585f3b4c05777ea41
Euphoriyy <96319862+Euphoriyy@users.noreply.github.com>
2025-06-19 00:46:55 -0400

    feat: make gcd function non-recursive/iterative

--- include/math.h
+++ include/math.h
@@ -2,6 +2,13 @@
 
 #include <cstdint>
 
+inline void swap(uint24_t &a, uint24_t &b)
+{
+    uint24_t temp = a;
+    a = b;
+    b = temp;
+}
+
 uint8_t gcd(uint24_t a, uint24_t b);
 
 struct Vector2
--- src/math.cpp
+++ src/math.cpp
@@ -1,3 +1,11 @@
 #include "../include/math.h"
 
-uint8_t gcd(uint24_t a, uint24_t b) { return !b ? a : gcd(b, a % b); }
+uint8_t gcd(uint24_t a, uint24_t b)
+{
+    while (b)
+    {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
