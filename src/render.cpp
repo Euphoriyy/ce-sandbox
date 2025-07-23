6534267c663b7124a96e676c0ca6da1da2319e91
Euphoriyy <96319862+Euphoriyy@users.noreply.github.com>
2025-07-23 16:37:11 -0400

    feat: add brush & eraser icons to UI

--- src/render.cpp
+++ src/render.cpp
@@ -179,9 +179,15 @@ void drawGUI()
     gfx_SetTextScale(1, 1);
 
     if (gameState.isDrawing)
+    {
         gfx_PrintString("DRAW");
+        gfx_TransparentSprite_NoClip(brush_icon, 45, 2);
+    }
     else if (gameState.isErasing)
+    {
         gfx_PrintString("ERASE");
+        gfx_TransparentSprite_NoClip(eraser_icon, 53, 2);
+    }
     else
         gfx_PrintString("IDLE");
 
